ec6288584a5e60b341e79608f8b8f1cb86bc3894
Carlos Leyton <leyton.severino@gmail.com>
2021-02-19 15:12:16 -0300

    orden de archivos
    falta dependencias por errores de vscode de trabajo, fuera de red minsal se corregirá

--- days-service/src/main/java/carlosleyton/daysservice/controller/DaysServiceApplication.java
+++ days-service/src/main/java/carlosleyton/daysservice/controller/DaysServiceApplication.java
@@ -2,7 +2,9 @@ package carlosleyton.daysservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class DaysServiceApplication {
 
@@ -17,9 +19,10 @@ public class DaysServiceApplication {
 
 	@Bean
 	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		@GetMapping("/list")
 		return args -> {
 			DaysCountry daysCountry = restTemplate.getForObject(
-					"https://api.covid19api.com/country/", DaysCountry.class);
+					"https://api.covid19api.com/country/{country}", DaysCountry.class);
 			log.info(daysCountry.toString());
 		};
 	}
